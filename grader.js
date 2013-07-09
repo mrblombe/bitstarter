3b91311499bdf96afa4cb2aa73af159d9d8f1902
Mikael Blomberg <mikael.blomberg@kolumbus.fi>
2013-07-09 21:56:31 +0000

    Modified checks.json and grader.js

--- grader.js
+++ grader.js
@@ -24,7 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var restler = require('restler');
+var HTMLFILE_DEFAULT = "indexs.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -42,6 +43,17 @@ var cheerioHtmlFile = function(htmlfile) {
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkUrlFile = function(urlfile, checksfile) {
+    $ = cheerio.load(urlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
@@ -63,12 +75,42 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>',
+                'Path to checks.json',
+                clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>',
+                'Path to index.html')
+        .option('-u, --url <html_url>',
+                'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson = "";
+    var outJson = "";
+    if (!program.file && !program.url) {
+        console.log("--file or --url is required. Exiting.");
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+      // do nothing
+    } else if(program.url) {
+      // handle 
+      restler.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) {
+          console.log("Unable to retrieve",
+                      program.url,
+                      "got error",
+                      result.messag.message,
+                      "instead. Exiting.");
+          process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        } else {
+          checkJson = checkUrlFile(result, program.checks);
+          outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+        }
+      });
+    } else {
+      // assumes program.file exists
+      checkJson = checkHtmlFile(program.file, program.checks);
+      outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
