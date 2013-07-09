767444b24aac8497a825ec897b46c14563e009cb
Mikael Blomberg <mikael.blomberg@kolumbus.fi>
2013-07-09 20:08:37 +0000

    Fixed encoding.

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 // syncronically read index file to cache when creating server
 // no need to use buffer as the content can be cached in this case
-var cachedIndex = fs.readFileSync('index.html', {"encoding": "utf-8"});
+var cachedIndex = fs.readFileSync('index.html', {"encoding": "utf8"});
 
 app.get('/', function(request, response) {
   response.send(cachedIndex);
