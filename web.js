eff8e21b95edb3137cd8798392db44376fc88358
Mikael Blomberg <mikael.blomberg@kolumbus.fi>
2013-07-09 19:59:14 +0000

    Modified web.js to read index.html to cache and send cached index on request.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+// syncronically read index file to cache when creating server
+// no need to use buffer as the content can be cached in this case
+var cachedIndex = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(cachedIndex);
 });
 
 var port = process.env.PORT || 5000;
